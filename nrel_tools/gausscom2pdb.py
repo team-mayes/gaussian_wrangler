f3207eef4a7f0792f3d7cb745c2330021afae065
Heather Mayes <hmayes@hmayes.com>
2019-06-26 13:49:31 -0600

    Initial gaussian com to pdb script

---
+++ nrel_tools/gausscom2pdb.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python
+"""
+Creates pdb data files from lammps data files, given a template pdb file.
+"""
+
+from __future__ import print_function
+import os
+import copy
+import logging
+import re
+import sys
+import argparse
+from nrel_tools.common import (InvalidDataError, warning, process_cfg, create_out_fname, list_to_file, process_pdb_tpl,
+                               HEAD_CONTENT, ATOMS_CONTENT, TAIL_CONTENT, PDB_FORMAT, NUM_ATOMS)
+
+try:
+    # noinspection PyCompatibility
+    from ConfigParser import ConfigParser, MissingSectionHeaderError
+except ImportError:
+    # noinspection PyCompatibility
+    from configparser import ConfigParser, MissingSectionHeaderError
+
+__author__ = 'hmayes'
+
+
+# Logging
+logger = logging.getLogger('gausscom2pdb')
+# logging.basicConfig(filename='gausscom2pdb.log', filemode='w', level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
+
+# Error Codes
+# The good status code
+GOOD_RET = 0
+INPUT_ERROR = 1
+IO_ERROR = 2
+INVALID_DATA = 3
+
+# Constants #
+
+# Config File Sections
+MAIN_SEC = 'main'
+
+# Config keys
+PDB_TPL_FILE = 'pdb_tpl_file'
+GAUSSCOM_FILES_FILE = 'gausscom_list_file'
+GAUSSCOM_FILES = 'gausscom_files_list'
+GAUSSCOM_FILE = 'gausscom_file'
+CENTER_ATOM = 'center_to_atom_num'
+
+OUT_BASE_DIR = 'output_directory'
+MAKE_DICT_BOOL = 'make_dictionary_flag'
+
+GAU_HEADER_PAT = re.compile(r"#.*")
+
+# data file info
+
+# Defaults
+DEF_CFG_FILE = 'gausscom2pdb.ini'
+# Set notation
+DEF_CFG_VALS = {GAUSSCOM_FILES_FILE: 'gausscom_list.txt',
+                OUT_BASE_DIR: None,
+                MAKE_DICT_BOOL: False,
+                GAUSSCOM_FILE: None,
+                }
+REQ_KEYS = {PDB_TPL_FILE: str,
+            }
+
+# For pdb template file processing
+SEC_HEAD = 'head_section'
+SEC_ATOMS = 'atoms_section'
+SEC_TAIL = 'tail_section'
+
+
+def read_cfg(f_loc, cfg_proc=process_cfg):
+    """
+    Reads the given configuration file, returning a dict with the converted values supplemented by default values.
+
+    :param f_loc: The location of the file to read.
+    :param cfg_proc: The processor to use for the raw configuration values.  Uses default values when the raw
+        value is missing.
+    :return: A dict of the processed configuration file's data.
+    """
+    config = ConfigParser()
+    good_files = config.read(f_loc)
+
+    if not good_files:
+        raise IOError('Could not read file {}'.format(f_loc))
+    main_proc = cfg_proc(dict(config.items(MAIN_SEC)), DEF_CFG_VALS, REQ_KEYS)
+
+    # To fix; have this as default!
+    main_proc[GAUSSCOM_FILES] = []
+    if os.path.isfile(main_proc[GAUSSCOM_FILES_FILE]):
+        with open(main_proc[GAUSSCOM_FILES_FILE]) as f:
+            for data_file in f:
+                main_proc[GAUSSCOM_FILES].append(data_file.strip())
+    if main_proc[GAUSSCOM_FILE] is not None:
+        main_proc[GAUSSCOM_FILES].append(main_proc[GAUSSCOM_FILE])
+    if len(main_proc[GAUSSCOM_FILES]) == 0:
+        raise InvalidDataError("No files to process: no '{}' specified and "
+                               "no list of files found for: {}".format(GAUSSCOM_FILE, main_proc[GAUSSCOM_FILES_FILE]))
+
+    return main_proc
+
+
+def parse_cmdline(argv):
+    """
+    Returns the parsed argument list and return code.
+    `argv` is a list of arguments, or `None` for ``sys.argv[1:]``.
+    """
+    if argv is None:
+        argv = sys.argv[1:]
+
+    # initialize the parser object:
+    parser = argparse.ArgumentParser(description='Creates pdb files from Gaussian input files, given a template pdb '
+                                                 'file.')
+    parser.add_argument("-c", "--config", help="The location of the configuration file in ini format. "
+                                               "The default file name is {}, located in the "
+                                               "base directory where the program as run.".format(DEF_CFG_FILE),
+                        default=DEF_CFG_FILE, type=read_cfg)
+    args = None
+    try:
+        args = parser.parse_args(argv)
+    except IOError as e:
+        warning("Problems reading file:", e)
+        parser.print_help()
+        return args, IO_ERROR
+    except (KeyError, InvalidDataError, MissingSectionHeaderError, SystemExit) as e:
+        if hasattr(e, 'code') and e.code == 0:
+            return args, GOOD_RET
+        warning(e)
+        parser.print_help()
+        return args, INPUT_ERROR
+
+    return args, GOOD_RET
+
+
+def process_gausscom_files(cfg, pdb_tpl_content):
+    # Don't want to change the original template data when preparing to print the new file:
+
+    for gausscom_file in cfg[GAUSSCOM_FILES]:
+        process_gausscom_file(cfg, gausscom_file, pdb_tpl_content)
+
+
+def process_gausscom_file(cfg, gausscom_file, pdb_tpl_content):
+    with open(gausscom_file) as d:
+        pdb_data_section = copy.deepcopy(pdb_tpl_content[ATOMS_CONTENT])
+        section = SEC_HEAD
+        atom_id = 0
+        lines_after_header = 4  # blank line, description, blank line, charge & multiplicity
+        atom_types = []
+
+        for line in d:
+            line = line.strip()
+            # not currently keeping anything from the header; just check num atoms
+            if section == SEC_HEAD:
+                if GAU_HEADER_PAT.match(line):
+                    continue
+                elif lines_after_header > 0:
+                    lines_after_header -= 1
+                    if lines_after_header == 0:
+                        section = SEC_ATOMS
+                    continue
+
+            elif section == SEC_ATOMS:
+                if len(line) == 0:
+                    continue
+                split_line = line.split()
+
+                # Keep as string; json save as string and this helps compare
+                atom_types.append(split_line[0])
+                pdb_data_section[atom_id][5:8] = map(float, split_line[1:4])
+                atom_id += 1
+                # Check after increment because the counter started at 0
+                if atom_id == pdb_tpl_content[NUM_ATOMS]:
+                    # Since the tail will come only from the template, nothing more is needed.
+                    break
+
+    # Now that finished reading the file, first make sure didn't  exit before reaching the desired number of atoms
+    if atom_id != pdb_tpl_content[NUM_ATOMS]:
+        raise InvalidDataError('In gausscom file: {}\n'
+                               '  found {} atoms, but pdb expects {} atoms'.format(gausscom_file, atom_id,
+                                                                                   pdb_tpl_content[NUM_ATOMS]))
+    f_name = create_out_fname(gausscom_file, ext='.pdb', base_dir=cfg[OUT_BASE_DIR])
+    list_to_file(pdb_tpl_content[HEAD_CONTENT] + pdb_data_section + pdb_tpl_content[TAIL_CONTENT],
+                 f_name, list_format=PDB_FORMAT)
+
+
+def main(argv=None):
+    # Read input
+    args, ret = parse_cmdline(argv)
+    if ret != GOOD_RET or args is None:
+        return ret
+
+    cfg = args.config
+
+    # Read template and data files
+    try:
+        pdb_tpl_content = process_pdb_tpl(cfg[PDB_TPL_FILE])
+        process_gausscom_files(cfg, pdb_tpl_content)
+    except IOError as e:
+        warning("Problems reading file:", e)
+        return IO_ERROR
+    except InvalidDataError as e:
+        warning("Problems reading data:", e)
+        return INVALID_DATA
+
+    return GOOD_RET  # success
+
+
+if __name__ == '__main__':
+    status = main()
+    sys.exit(status)
---
+++ tests/test_gausscom2pdb.py
@@ -0,0 +1,139 @@
+import unittest
+import os
+from nrel_tools.gausscom2pdb import main
+from nrel_tools.common import diff_lines, silent_remove, capture_stdout, capture_stderr
+import logging
+
+# logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+DISABLE_REMOVE = logger.isEnabledFor(logging.DEBUG)
+
+__author__ = 'hmayes'
+
+TEST_DIR = os.path.dirname(__file__)
+MAIN_DIR = os.path.dirname(TEST_DIR)
+DATA_DIR = os.path.join(os.path.dirname(__file__), 'test_data')
+SUB_DATA_DIR = os.path.join(DATA_DIR, 'gausscom2pdb')
+DEF_INI = os.path.join(SUB_DATA_DIR, 'gausscom2pdb.ini')
+# TYPO_INI = os.path.join(SUB_DATA_DIR, 'gauscom2pdb_typo.ini')
+# MISS_INI = os.path.join(SUB_DATA_DIR, 'gauscom2pdb_miss.ini')
+# NO_FILES_INI = os.path.join(SUB_DATA_DIR, 'gauscom2pdb_no_files.ini')
+# DIFF_ATOM_NUM_INI = os.path.join(SUB_DATA_DIR, 'gauscom2pdb_diff_atom_num.ini')
+# DIFF_ATOM_NUM_DICT_INI = os.path.join(SUB_DATA_DIR, 'gauscom2pdb_dict_diff_atom_num.ini')
+# CUTOFF_DATA_INI = os.path.join(SUB_DATA_DIR, 'gauscom2pdb_cutoff.ini')
+# GHOST_LIST_INI = os.path.join(SUB_DATA_DIR, 'gauscom2pdb_ghost_list.ini')
+# NO_DICT_INI = os.path.join(SUB_DATA_DIR, 'gauscom2pdb_no_dict.ini')
+# GLU_INI = os.path.join(SUB_DATA_DIR, 'gauscom2pdb_glu.ini')
+# GLU_DICT_INI = os.path.join(SUB_DATA_DIR, 'gauscom2pdb_glu_dict.ini')
+#
+# NOT_INI = os.path.join(SUB_DATA_DIR, 'glue_hm_cutoff.data')
+#
+# PDB_TPL = os.path.join(SUB_DATA_DIR, 'glue_hm_tpl.pdb')
+# # noinspection PyUnresolvedReferences
+# PDB_TPL_OUT = os.path.join(SUB_DATA_DIR, 'reproduced_tpl.pdb')
+
+# noinspection PyUnresolvedReferences
+PDB_OUT = os.path.join(SUB_DATA_DIR, 'pet_cp1_def2_end.pdb')
+GOOD_PDB_OUT = os.path.join(SUB_DATA_DIR, 'pet_cp1_def2_end_good.pdb')
+
+# # noinspection PyUnresolvedReferences
+# GLU_OUT = os.path.join(SUB_DATA_DIR, '0.625_20c_reorder_retype_548990.pdb')
+# GOOD_GLU_OUT = os.path.join(SUB_DATA_DIR, '0.625_20c_reorder_retype_548990_good.pdb')
+#
+# DEF_DICT_OUT = os.path.join(MAIN_DIR, 'atom_dict.json')
+# GOOD_DICT = os.path.join(SUB_DATA_DIR, 'atom_dict_good.json')
+
+
+class Testgauscom2pdbNoOut(unittest.TestCase):
+    # These all test failure cases
+    def testNoArgs(self):
+        with capture_stderr(main, []) as output:
+            self.assertTrue("WARNING:  Problems reading file: Could not read file" in output)
+        with capture_stdout(main, []) as output:
+            self.assertTrue("optional arguments" in output)
+
+    # def testTypoIni(self):
+    #     with capture_stderr(main, ["-c", TYPO_INI]) as output:
+    #         self.assertTrue("Unexpected key" in output)
+    #
+    # def testMissReqKeyIni(self):
+    #     with capture_stderr(main, ["-c", MISS_INI]) as output:
+    #         self.assertTrue("Missing config val for key 'pdb_tpl_file'" in output)
+    #
+    # def testNoFiles(self):
+    #     with capture_stderr(main, ["-c", NO_FILES_INI]) as output:
+    #         self.assertTrue("No files to process" in output)
+    #
+    # def testNoFilesInList(self):
+    #     with capture_stderr(main, ["-c", GHOST_LIST_INI]) as output:
+    #         self.assertTrue("Problems reading file" in output)
+    #
+    # def testDiffAtomNum(self):
+    #     with capture_stderr(main, ["-c", DIFF_ATOM_NUM_INI]) as output:
+    #         self.assertTrue("Mismatched numbers of atoms" in output)
+    #
+    # def testDiffAtomNumDict(self):
+    #     with capture_stderr(main, ["-c", DIFF_ATOM_NUM_DICT_INI]) as output:
+    #         self.assertTrue("Mismatched numbers of atoms" in output)
+    #
+    # def testCutoffData(self):
+    #     # An incomplete data file (as if stopped during copying)
+    #     with capture_stderr(main, ["-c", CUTOFF_DATA_INI]) as output:
+    #         self.assertTrue("atoms, but found" in output)
+    #
+    # def testNotIni(self):
+    #     # gracefully fail if give the wrong file to the -c option
+    #     test_input = ["-c", NOT_INI]
+    #     if logger.isEnabledFor(logging.DEBUG):
+    #         main(test_input)
+    #     with capture_stderr(main, test_input) as output:
+    #         self.assertTrue("WARNING:  File contains no section headers" in output)
+    #
+    # def testNoDictFound(self):
+    #     with capture_stderr(main, ["-c", NO_DICT_INI]) as output:
+    #         self.assertTrue("The program will continue without checking atom types" in output)
+
+    def testHelp(self):
+        test_input = ['-h']
+        if logger.isEnabledFor(logging.DEBUG):
+            main(test_input)
+        with capture_stderr(main, test_input) as output:
+            self.assertFalse(output)
+        with capture_stdout(main, test_input) as output:
+            self.assertTrue("optional arguments" in output)
+
+
+class Testgauscom2pdb(unittest.TestCase):
+    # These test/demonstrate different options
+    def testDefIni(self):
+        test_input = ["-c", DEF_INI]
+        if logger.isEnabledFor(logging.DEBUG):
+            main(test_input)
+        try:
+            main(test_input)
+            self.assertFalse(diff_lines(PDB_OUT, GOOD_PDB_OUT))
+        finally:
+            # silent_remove(PDB_TPL_OUT)
+            silent_remove(PDB_OUT, disable=DISABLE_REMOVE)
+
+    # def testGlu(self):
+    #     try:
+    #         gauscom2pdb.main(["-c", GLU_INI])
+    #         self.assertFalse(diff_lines(GLU_OUT, GOOD_GLU_OUT))
+    #     finally:
+    #         silent_remove(PDB_TPL_OUT)
+    #         silent_remove(GLU_OUT)
+    #
+    # def testGluDict(self):
+    #     try:
+    #         gauscom2pdb.main(["-c", GLU_DICT_INI])
+    #         self.assertFalse(diff_lines(GLU_OUT, GOOD_GLU_OUT))
+    #         with open(DEF_DICT_OUT, 'r') as d_file:
+    #             dict_test = json.load(d_file)
+    #         with open(GOOD_DICT, 'r') as d_file:
+    #             dict_good = json.load(d_file)
+    #         self.assertEqual(dict_test, dict_good)
+    #     finally:
+    #         silent_remove(PDB_TPL_OUT)
+    #         silent_remove(GLU_OUT)
+    #         silent_remove(DEF_DICT_OUT)
