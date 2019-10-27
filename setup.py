a1a10d5f3154d65991f1aa940e0897a7b0820fb4
Heather B Mayes <hmayes@hmayes.com>
2019-10-27 10:47:38 -0600

    minor cleanup

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(
                                       'check_gauss = gaussian_wrangler.check_gauss:main',
                                       'goodvibes_helper = gaussian_wrangler.goodvibes_helper:main',
                                       'plot_steps = gaussian_wrangler.plot_steps:main',
-                                      'eagle_gauss_setup = gaussian_wrangler.eagle_gauss_setup:main'
                                       ],
                   },     package_dir={'gaussian_wrangler': 'gaussian_wrangler'},
 
