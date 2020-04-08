2ba96fcfcd3dca8be7e8aea7fd186ede86800322
Heather B Mayes <hmayes@hmayes.com>
2020-04-08 10:54:06 -0600

    now remove differences between rm062x, um062x, and m062x

--- gaussian_wrangler/goodvibes_helper.py
+++ gaussian_wrangler/goodvibes_helper.py
@@ -321,18 +321,23 @@ def check_gausslog_fileset(file_set, good_vibes_check, results_dict):
                 if i > 160:
                     break
         if index == 0:
-            solvent = str(results_dict[base_name][SOLV])
-            func = str(gauss_result.getFunctional())
-            basis = str(gauss_result.getBasisSet())
+            solvent = str(results_dict[base_name][SOLV]).lower()
+            func = str(gauss_result.getFunctional()).lower()
+            if func.startswith("r") or func.startswith("u"):
+                func = func[1:]
+            basis = str(gauss_result.getBasisSet()).lower()
             gauss_ver = file_gauss_ver
         else:
-            if str(gauss_result.getSolvent()).lower() != solvent.lower():
+            if str(gauss_result.getSolvent()).lower() != solvent:
                 raise InvalidDataError("Different solvents ({}, {}) found for file set: "
                                        "{}".format(solvent, gauss_result.getSolvent(), file_set))
-            if str(gauss_result.getFunctional()).lower() != func.lower():
+            current_func = str(gauss_result.getFunctional()).lower()
+            if current_func.startswith("u") or current_func.startswith("r"):
+                current_func = current_func[1:]
+            if current_func != func:
                 raise InvalidDataError("Different functionals ({}, {}) found for file set: "
                                        "{}".format(func, gauss_result.getFunctional(), file_set))
-            if str(gauss_result.getBasisSet()).lower() != basis.lower():
+            if str(gauss_result.getBasisSet()).lower() != basis:
                 raise InvalidDataError("Different basis sets ({}, {}) found for file set: "
                                        "{}".format(basis, gauss_result.getBasisSet(), file_set))
             if gauss_ver != file_gauss_ver:
